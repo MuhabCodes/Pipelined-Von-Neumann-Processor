c88345e9d08dfc7db8b0e136eddd8c2553a52e06
Muhab <muhab.hossam13@gmail.com>
2022-05-29 03:36:28 +0200

    Updated the opcode for some instructions

--- src/assembler/assembler.py
+++ src/assembler/assembler.py
@@ -37,7 +37,10 @@ def parse_instruction(instruction: string):
 	print(instruction)
 	#part.append(f'{(int(instruction[1][1],16)):03b}')
 	op = part[0]
-	if op in (opcode["NOT"], opcode["INC"], opcode["OUT"], opcode["IN"], opcode["PUSH"], opcode["POP"]):
+	if op in (opcode["NOT"], opcode["INC"]):
+		part.append(f'{(int(instruction[1][1],16)):03b}')
+		part.append(f'{(int(instruction[1][1],16)):03b}')
+	if op in (opcode["OUT"], opcode["IN"], opcode["PUSH"], opcode["POP"]):
 		part.append(f'{(int(instruction[1][1],16)):03b}')
 	if op in (opcode["LDD"], opcode["STD"]):
 		# swap offset value and 2nd register
