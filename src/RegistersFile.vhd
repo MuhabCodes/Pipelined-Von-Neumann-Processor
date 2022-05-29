45ec56a8be607970a5301653d74dd72a0c34a703
Muhab <muhab.hossam13@gmail.com>
2022-05-30 01:02:12 +0200

    non-sequential register flags

--- src/RegistersFile.vhd
+++ src/RegistersFile.vhd
@@ -31,15 +31,26 @@ signal regOut: reg_file;
 signal wReg: std_logic_vector(7 downto 0) := (others=>'0');
 
 begin
-   	process (clk)
-	begin
-		wReg <= (others => '0');
-		wReg(to_integer(unsigned(write_reg))) <= reg_write;
-	end process;
+   	--process (clk)
+	--begin
+	--	wReg <= (others => '0');
+	--	wReg(to_integer(unsigned(write_reg))) <= reg_write;
+	-- end process;
+    wReg <= 
+         "10000000" when to_integer(unsigned(write_reg)) = 7 and reg_write = '1'
+    else "01000000" when to_integer(unsigned(write_reg)) = 6 and reg_write = '1'
+    else "00100000" when to_integer(unsigned(write_reg)) = 5 and reg_write = '1'
+    else "00010000" when to_integer(unsigned(write_reg)) = 4 and reg_write = '1'
+    else "00001000" when to_integer(unsigned(write_reg)) = 3 and reg_write = '1'
+    else "00000100" when to_integer(unsigned(write_reg)) = 2 and reg_write = '1'
+    else "00000010" when to_integer(unsigned(write_reg)) = 1 and reg_write = '1'
+    else "00000001" when to_integer(unsigned(write_reg)) = 0 and reg_write = '1'
+    else "00000000";
 
 	loop1: for i in 0 to 7 generate
 		regX: reg generic map(32) port map(write_data, regOut(i), wReg(i), clk, rst);
 	end generate;
+
     read_data1 <= regOut(to_integer(unsigned(Rsrc1)));
     read_data2 <= regOut(to_integer(unsigned(Rsrc2)));
 
